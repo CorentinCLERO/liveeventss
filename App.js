4ebd983160715f904ba2910f0685d4df88ff8f6f
CorentinCLERO <corentin.clero@epsi.fr>
2023-04-25 13:56:36 +0200

    ajout partenaire et en cours de changement de la page programmation

--- App.js
+++ App.js
@@ -6,16 +6,54 @@ import Accueil from './components/Accueil';
 import Programmation from './components/Programmation';
 import Carte from './components/Carte';
 import Billeterie from './components/Billeterie';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const screenWidth = Dimensions.get('window').width;
 const Drawer = createDrawerNavigator();
 
 export default function App() {
+  const [posts, setPosts] = useState([]);
+  const [artistes, setArtistes] = useState([])
+  const [localisations, setLocalisations] = useState([])
+
+  useEffect(() => {
+    let postsURL = "http://cchost.freeboxos.fr/LiveEvents/wp-json/wp/v2/posts?_embed&per_page=100"
+    axios.get(postsURL).then((res) => {
+      setPosts(JSON.parse(res.request._response))
+    }).catch((error) => {
+      console.log('ceci est une erreur ' + error)
+    });
+  }, [])
+
+  useEffect(() => {
+    const filteredPosts = posts.filter(post => {
+      const terms = post._embedded['wp:term'][0];
+      if (terms && terms.length > 0) {
+        return terms.some(term => term.slug === 'artiste');
+      }
+      return false;
+    });
+    setArtistes(filteredPosts);
+  }, [posts]);
+
+  useEffect(() => {
+    const filteredPosts = posts.filter(post => {
+      const terms = post._embedded['wp:term'][0];
+      if (terms && terms.length > 0) {
+        return terms.some(term => term.slug === 'localisations');
+      }
+      return false;
+    });
+    setLocalisations(filteredPosts);
+    console.log(localisations)
+  }, [posts]);
+
 
   return (
     <NavigationContainer>
       <Drawer.Navigator
-        initialRouteName="Acceuil"
+        initialRouteName="Programmation"
         screenOptions={{
           headerTitle: () => (
             <View style={styles.header}>
@@ -28,10 +66,16 @@ export default function App() {
               </View>
             </View>
           ),
+          headerStyle: {
+            borderBottomRightRadius: 20,
+            borderBottomLeftRadius: 20,
+          },
         }}
       >
         <Drawer.Screen name="Accueil" component={Accueil} />
-        <Drawer.Screen name="Programmation" component={Programmation} />
+        <Drawer.Screen name="Programmation">
+          {(props) => <Programmation {...props} posts={artistes} />}
+        </Drawer.Screen>
         <Drawer.Screen name="Carte" component={Carte} />
         <Drawer.Screen name="Billeterie" component={Billeterie} />
       </Drawer.Navigator>
@@ -53,8 +97,8 @@ const styles = StyleSheet.create({
     paddingLeft: screenWidth * 0.055
   },
   headerImageLogoAdjust: {
-    width: 40,
-    height: 40,
+    width: screenWidth * 0.1,
+    height: screenWidth * 0.1,
     resizeMode: 'contain',
     alignItems: 'center',
   },
@@ -62,15 +106,15 @@ const styles = StyleSheet.create({
     flexDirection: 'row'
   },
   headerImageLeftNotif: {
-    width: 30,
-    height: 30,
+    width: screenWidth * 0.08,
+    height: screenWidth * 0.08,
     resizeMode: 'contain',
     alignItems: 'center',
     marginRight: 15
   },
   headerImageLeftDrapeau: {
-    width: 25,
-    height: 25,
+    width: screenWidth * 0.06,
+    height: screenWidth * 0.06,
     resizeMode: 'contain',
     alignItems: 'center',
     marginTop: 'auto'
--- components/Programmation.js
+++ components/Programmation.js
@@ -1,13 +1,52 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { FlatList } from 'react-native';
+import { Dimensions } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
-const Programmation = () => {
+const screenWidth = Dimensions.get('window').width;
+
+const Programmation = ({ posts }) => {
+
+
 
     return (
-        <Text>
-            Page programmation en travaux
-        </Text>
+        <View>
+            <Text>
+                Page programmation en travaux
+            </Text>
+            <View style={styles.topBar}>
+                <Image source={require('../icones/rechercher.png')} style={styles.icone} />
+                <View style={styles.text}><Text>SAMEDI</Text><Text>10 Juin</Text></View>
+                <View style={styles.text}><Text>DIMANCHE</Text><Text>11 Juin</Text></View>
+                <Image source={require('../icones/filtre.png')} style={styles.icone} />
+            </View>
+            <FlatList
+                data={posts}
+                renderItem={({ item }) => <Text>{item.id} {item.title.rendered} {item._embedded['wp:term'][0][0].slug}</Text>}
+                keyExtractor={item => item.id}
+                style={styles.list}
+            />
+        </View>
     );
 };
 
 export default Programmation;
+
+const styles = StyleSheet.create({
+    topBar: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: '#C0C0C0',
+    },
+    icone: {
+        height: screenWidth * 0.1,
+        width: screenWidth * 0.1,
+        marginHorizontal: 5,
+    },
+    text: {
+        alignItems: 'center',
+    },
+    list: {
+        marginBottom: 60,
+    },
+})
